32b4e7a3551348ddc579e82b9d5c0f80f7f45e70
Pawel Haracz <haracz5@live.com>
2020-07-11 15:24:26 +0200

    Add await bool

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -5,13 +5,23 @@ namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool val) => new BoolAwaiter(val);
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool _value;
+
+        public BoolAwaiter(bool value)
+        {
+            _value = value;
+        }
+        public bool IsCompleted => true;
         public void OnCompleted(Action continuation)
         {
             throw new NotImplementedException();
         }
+
+        public bool GetResult() => _value;
     }
 }
