e378df9cd04bf159e0e1917f6c185352f37871bd
Pawel Haracz <haracz5@live.com>
2020-08-12 09:34:12 +0200

    Homework 7

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -9,10 +9,25 @@ namespace Synchronization.Core
      *
      * Created region may be system-wide or not, depending on the constructor parameter.
      */
-    public class NamedExclusiveScope : IDisposable
+    public sealed class NamedExclusiveScope : IDisposable
     {
+        private readonly Mutex _mutex;
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
+            var mutexName = $"{(isSystemWide ? "Global" : "Local")}\\{name}"; 
+            _mutex = new Mutex(false, mutexName, out var createdNew);
+            if (!createdNew)
+            {
+                throw new InvalidOperationException($"Unable to get a global lock {name}.");
+            }
+            
+            _mutex.WaitOne();
+        }
+
+        public void Dispose()
+        {
+            _mutex.ReleaseMutex();
+            _mutex?.Dispose();
         }
     }
 }
